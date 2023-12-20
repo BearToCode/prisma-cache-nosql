f01cfd88c5c996cb91f1d385cb362a0d73ea7d0d
BearToCode <davidebasso03@gmail.com>
2023-12-20 22:05:39 +0100

    fix(build): dist not including js

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,32 @@ import typescript from '@rollup/plugin-typescript';
 import { dts } from 'rollup-plugin-dts';
 
 /** @type {import('rollup').RollupOptions} */
-export default {
-	input: './src/index.ts',
-	external: ['@prisma/client', 'chalk', 'object-hash'],
-	output: {
-		dir: 'dist',
-		exports: 'named',
-		preserveModules: true,
-		sourcemap: true
+export default [
+	{
+		input: './src/index.ts',
+		external: ['@prisma/client', 'chalk', 'object-hash'],
+		output: {
+			dir: 'dist',
+			preserveModules: true,
+			exports: 'named',
+			sourcemap: true
+		},
+		plugins: [typescript()]
 	},
-	plugins: [
-		typescript(),
-		dts({
-			tsconfig: './tsconfig.json'
-		})
-	]
-};
+	{
+		input: './src/index.ts',
+		output: {
+			dir: 'dist',
+			exports: 'named',
+			preserveModules: true,
+			sourcemap: true
+		},
+		plugins: [
+			dts(),
+			// Use typescript only to resolve aliases
+			typescript({
+				noEmit: true
+			})
+		]
+	}
+];
